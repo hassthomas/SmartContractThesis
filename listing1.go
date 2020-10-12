a1ca1a079543a2c3c1acb93635672d24856c2bed
hassthomas <44865009+hassthomas@users.noreply.github.com>
2020-10-12 18:27:27 +0200

    Add files via upload

---
+++ listing1.go
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+type Chaincode struct {
+}
+
+// Init is called when the chaincode is instantiated by the blockchain network.
+func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+	fmt.Println("Init()", fcn, params)
+	return shim.Success(nil)
+}
+
+// Invoke is called as a result of an application request to run the chaincode.
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+	fmt.Println("Invoke()", fcn, params)
+	return shim.Success(nil)
+}
+
+func listing1() {
+	// User predicts a number
+	pred := arg[0]
+
+	// Answer
+	rand.Seed(seed)
+	sel := rand.Intn(10)
+
+	if pred == sel {
+		PayPrizeToUser(user, prize)
+	}
+}
+
+func main() {
+	err := shim.Start(new(Chaincode))
+	if err != nil {
+		panic(err)
+	}
+}
