0c6571cecd0e4350bf1393ef6543c1e308c6879f
hassthomas <44865009+hassthomas@users.noreply.github.com>
2020-10-12 21:15:53 +0200

    Update main.go

--- main.go
+++ main.go
@@ -92,6 +92,48 @@ func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
     return string(value), nil
 }
 
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+// Chaincode is the definition of the chaincode structure.
+type Chaincode struct {
+}
+
+// Init is called when the chaincode is instantiated by the blockchain network.
+func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+	fmt.Println("Init()", fcn, params)
+	return shim.Success(nil)
+}
+
+// Invoke is called as a result of an application request to run the chaincode.
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+	fmt.Println("Invoke()", fcn, params)
+	return shim.Success(nil)
+}
+
+func listing1() {
+	// User predicts a number
+	pred := arg[0]
+	// Answer
+	rand.Seed(seed)
+	sel := rand.Intn(10)
+	if pred == sel {
+		// PayPrizeToUser(user, prize)
+	}
+}	
+
 // main function starts up the chaincode in the container during instantiate
 func main() {
     err := shim.Start(new(SampleChaincode))
