ce300ab4215a5ecedcab459992335bd3aa8e3a41
hassthomas <44865009+hassthomas@users.noreply.github.com>
2020-10-13 10:41:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -91,18 +91,7 @@ func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
     }
     return string(value), nil
 }
-
-func listing1() {
-	// User predicts a number
-	pred := arg[0]
-	// Answer
-	rand.Seed(seed)
-	sel := rand.Intn(10)
-	if pred == sel {
-		// PayPrizeToUser(user, prize)
-		}
 	
-	}	
 
 // main function starts up the chaincode in the container during instantiate
 func main() {
@@ -113,5 +102,4 @@ func main() {
         fmt.Println("SampleChaincode successfully started")
     }
 	
-	listing1();
 }
